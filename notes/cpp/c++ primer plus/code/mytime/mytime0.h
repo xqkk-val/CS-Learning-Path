107d32ca2edf319cc1a27889da9873a456f8c3ba
Hitchcock <1597614665@qq.com>
2026-06-12 15:16:11 +0800

    vault backup: 2026-06-12 15:16:11

---
+++ notes/cpp/c++ primer plus/code/mytime/mytime0.h
@@ -0,0 +1,18 @@
+#ifndef MYTIME0_H_
+#define MYTIME0_H_
+
+class Time
+{private:
+    int hours;
+    int minutes;
+public:
+    Time();
+    Time(int h, int m = 0);
+    
+
+
+}
+
+
+
+#endif MYTIME0_H_
